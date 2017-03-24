50ee96dbc54980d8d5515e730b424f6b409746a7
Sergio David Torres Leon <sergio_51red@hotmail.com>
2017-03-24 17:57:17 -0500

    Add: Fonts-Awesome 4.7.0

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,5 @@ const { mix } = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .styles(['resources/assets/plugins/font-awesome/css/font-awesome.min.css'], 'public/css/vendor.css');
